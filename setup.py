e7f66335409c136fb8436c112bb6a8dd5dbc8f40
JL HSIEH <jirlong@gmail.com>
2024-03-16 22:52:02 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='lib2048',
-    version='0.1.0',
-    url='https://github.com/lab2048/lib2048',
+    name='tm2048',
+    version='0.1.1',
+    url='https://github.com/lab2048/tm2048',
     author='Jilung Hsieh',
     author_email='jirlong@gmail.com',
     description='Tools for my lab(2048)',
     packages=find_packages(),    
-    install_requires=[
-        'pandas>=2.0',
-    ],
+    install_requires=[],
 )
